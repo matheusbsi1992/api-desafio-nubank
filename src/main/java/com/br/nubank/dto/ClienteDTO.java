a564bc64459ad1b0db1b6a013b25ee58b2136146
matheusbsi1992 <matheusbsi1992@gmail.com>
2025-09-11 15:42:58 -0300

    commit 2.0

--- src/main/java/com/br/nubank/dto/ClienteDTO.java
+++ src/main/java/com/br/nubank/dto/ClienteDTO.java
@@ -1,21 +1,17 @@
 package com.br.nubank.dto;
 
 import com.br.nubank.model.Cliente;
-import com.br.nubank.model.Contato;
+
 import com.br.nubank.validacao.clientecontato.ValidarErrorCliente;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
